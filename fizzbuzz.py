5260343fc76993088604a9a6e917b2fc8db0431e
ovenbakedgoods <63219029+ovenbakedgoods@users.noreply.github.com>
2020-04-06 02:14:53 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -20,4 +20,22 @@ class fizzbuzz:
                 for x in my_fizz_buzz_list:
                     print(x)
 
-    fizz_buzz(23)
+    fizz_buzz(3)
+
+class Solution(object):
+   def fizzBuzz(self, n):
+         """
+      :type n: int
+      :rtype: List[str]
+      """
+      result = []
+      for i in range(1,n+1):
+         if i% 3== 0 and i%5==0:
+            result.append("FizzBuzz")
+         elif i %3==0:
+            result.append("Fizz")
+         elif i% 5 == 0:
+            result.append("Buzz")
+         else:
+            result.append(str(i))
+      return result
