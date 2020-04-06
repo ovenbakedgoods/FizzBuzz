129a5bb71bbedd264b3fd9f959c02fe8100b0844
ovenbakedgoods <63219029+ovenbakedgoods@users.noreply.github.com>
2020-04-06 02:12:22 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,23 @@
 class fizzbuzz:
 
-  def fizz_buzz(number):
-    my_fizz_buzz_list = []
-    for i in range(number):
 
-      if i % 3 == 0:
-        my_fizz_buzz_list.append("fizz")
+    def fizz_buzz(number):
+        my_fizz_buzz_list = []
+        for i in range(number):
 
-      elif i % 5 == 0:
-        my_fizz_buzz_list.append("buzz")
+            if i % 3 == 0:
+                my_fizz_buzz_list.append("fizz")
 
-      elif i % 3 == 0 and i % 5 == 0:
-        my_fizz_buzz_list.append("fizzbuzz")
+            elif i % 5 == 0:
+                my_fizz_buzz_list.append("buzz")
 
-      else:
-          my_fizz_buzz_list.append(i)
+            elif i % 3 == 0 and i % 5 == 0:
+                my_fizz_buzz_list.append("fizzbuzz")
 
-      for x in my_fizz_buzz_list:
-        print(x)
+            elif i % 3 != 0 and i % 5 != 0:
+                my_fizz_buzz_list.append(str(i))
+
+                for x in my_fizz_buzz_list:
+                    print(x)
 
     fizz_buzz(23)
